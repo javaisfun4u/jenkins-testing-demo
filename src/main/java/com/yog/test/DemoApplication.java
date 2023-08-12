da089152a7e7319b583edb0ffbe9c8745c5309f4
P YOGANARASIMHA REDDY <javaisfun4u@gmail.com>
2023-08-12 22:11:32 +0530

    third commit

--- src/main/java/com/yog/test/DemoApplication.java
+++ src/main/java/com/yog/test/DemoApplication.java
@@ -19,7 +19,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		log.info("App executed............");
-
+		log.info("App executed Second............");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
